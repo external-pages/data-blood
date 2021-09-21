bf5097241c7821ef04e3c724b3c2e0f7ed87f50f
anmeisel <aanameisell@gmail.com>
2021-09-21 17:19:15 +0100

    all sentences

--- js/data-blood.js
+++ js/data-blood.js
@@ -7,28 +7,28 @@ $(document).ready(function() {
   //   $(this).toggleClass('shadow')
   //   $('#container2').toggle()
   // })
-  $('.tv').click(function() {
-    $(this).toggleClass('shadow')
-    $('.manifesto3').toggle()
-  })
-  $('.ipad-table').click(function() {
-    $(this).toggleClass('shadow')
-    $('.manifesto4').toggle()
-  })
-  $('.arch-right').click(function() {
-    $(this).toggleClass('shadow')
-    $('.manifesto5').toggle()
-  })
-  $('.flower-table').click(function() {
-    $(this).toggleClass('shadow')
-    $('.manifesto6').toggle()
-  })
-  $('.arch-left').click(function() {
-    $(this).toggleClass('shadow')
-    $('.manifesto7').toggle()
-  })
-  $('.plant-table').click(function() {
-    $(this).toggleClass('shadow')
-    $('.manifesto8').toggle()
-  })
+  // $('.tv').click(function() {
+  //   $(this).toggleClass('shadow')
+  //   $('.manifesto3').toggle()
+  // })
+  // $('.ipad-table').click(function() {
+  //   $(this).toggleClass('shadow')
+  //   $('.manifesto4').toggle()
+  // })
+  // $('.arch-right').click(function() {
+  //   $(this).toggleClass('shadow')
+  //   $('.manifesto5').toggle()
+  // })
+  // $('.flower-table').click(function() {
+  //   $(this).toggleClass('shadow')
+  //   $('.manifesto6').toggle()
+  // })
+  // $('.arch-left').click(function() {
+  //   $(this).toggleClass('shadow')
+  //   $('.manifesto7').toggle()
+  // })
+  // $('.plant-table').click(function() {
+  //   $(this).toggleClass('shadow')
+  //   $('.manifesto8').toggle()
+  // })
 })
