f04e19c72c4115367d38efb01363d4bc3ca3596e
anmeisel <aanameisell@gmail.com>
2021-09-21 14:50:06 +0100

    first commit no. 2

---
+++ js/data-blood.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+  $(".funghi-table").click(function(){
+    $(this).toggleClass("shadow")
+    $(".manifesto1").toggle()
+  });
+  $(".heart-table").click(function(){
+    $(this).toggleClass("shadow")
+    $(".manifesto2").toggle()
+  });
+  $(".tv").click(function(){
+    $(this).toggleClass("shadow")
+    $(".manifesto3").toggle()
+  });
+  $(".ipad-table").click(function(){
+    $(this).toggleClass("shadow")
+    $(".manifesto4").toggle()
+  });
+  $(".arch-right").click(function(){
+    $(this).toggleClass("shadow")
+    $(".manifesto5").toggle()
+  });
+  $(".flower-table").click(function(){
+    $(this).toggleClass("shadow")
+    $(".manifesto6").toggle()
+  });
+  $(".arch-left").click(function(){
+    $(this).toggleClass("shadow")
+    $(".manifesto7").toggle()
+  });
+  $(".plant-table").click(function(){
+    $(this).toggleClass("shadow")
+    $(".manifesto8").toggle()
+  });
+});
