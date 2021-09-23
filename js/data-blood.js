b4a8a12a677473f308883e6b1b64b94431ee2f77
anmeisel <aanameisell@gmail.com>
2021-09-23 15:22:03 +0100

    colours

--- js/data-blood.js
+++ js/data-blood.js
@@ -1,34 +1,10 @@
 $(document).ready(function() {
-  // $('.funghi-table').click(function() {
-  //   $(this).toggleClass('shadow')
-  //   $('#container1').toggle()
-  // })
-  // $('.heart-table').click(function() {
-  //   $(this).toggleClass('shadow')
-  //   $('#container2').toggle()
-  // })
-  // $('.tv').click(function() {
-  //   $(this).toggleClass('shadow')
-  //   $('.manifesto3').toggle()
-  // })
-  // $('.ipad-table').click(function() {
-  //   $(this).toggleClass('shadow')
-  //   $('.manifesto4').toggle()
-  // })
-  // $('.arch-right').click(function() {
-  //   $(this).toggleClass('shadow')
-  //   $('.manifesto5').toggle()
-  // })
-  // $('.flower-table').click(function() {
-  //   $(this).toggleClass('shadow')
-  //   $('.manifesto6').toggle()
-  // })
-  // $('.arch-left').click(function() {
-  //   $(this).toggleClass('shadow')
-  //   $('.manifesto7').toggle()
-  // })
-  // $('.plant-table').click(function() {
-  //   $(this).toggleClass('shadow')
-  //   $('.manifesto8').toggle()
-  // })
+  $('#tv-video')[0].play()
+  $('.color').hide()
+  $('.image').on('click', function() {
+    $(this)
+      .parent()
+      .children('.color')
+      .show()
+  })
 })
